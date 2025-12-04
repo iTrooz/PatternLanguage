93c7a137b178a1462d53ca5840c2c9fb0e7b3e4f
iTrooz <hey@itrooz.fr>
2025-12-04 15:45:14 +0100

    no static

--- lib/include/pl/core/errors/runtime_errors.hpp
+++ lib/include/pl/core/errors/runtime_errors.hpp
@@ -7,26 +7,24 @@ namespace pl::ptrn { class Pattern; }
 
 namespace pl::core::err {
 
-    namespace {
-        class EvaluatorError : public RuntimeError<Location> {
-        public:
-            EvaluatorError(u32 errorCode, std::string title) noexcept :
-                    RuntimeError(errorCode, std::move(title)) { }
-        };
-    }
+    class EvaluatorError : public RuntimeError<Location> {
+    public:
+        EvaluatorError(u32 errorCode, std::string title) noexcept :
+                RuntimeError(errorCode, std::move(title)) { }
+    };
 
-    const static inline EvaluatorError E0001(1, "Evaluator bug.");
-    const static inline EvaluatorError E0002(2, "Math expression error.");
-    const static inline EvaluatorError E0003(3, "Variable error.");
-    const static inline EvaluatorError E0004(4, "Type error.");
-    const static inline EvaluatorError E0005(5, "Placement error.");
-    const static inline EvaluatorError E0006(6, "Array index error.");
-    const static inline EvaluatorError E0007(7, "Limit error.");
-    const static inline EvaluatorError E0008(8, "Attribute error.");
-    const static inline EvaluatorError E0009(9, "Function error.");
-    const static inline EvaluatorError E0010(10, "Control flow error.");
-    const static inline EvaluatorError E0011(11, "Memory error.");
-    const static inline EvaluatorError E0012(12, "Built-in function error.");
-    const static inline EvaluatorError E0013(13, "Ambiguity error.");
+    const inline EvaluatorError E0001(1, "Evaluator bug.");
+    const inline EvaluatorError E0002(2, "Math expression error.");
+    const inline EvaluatorError E0003(3, "Variable error.");
+    const inline EvaluatorError E0004(4, "Type error.");
+    const inline EvaluatorError E0005(5, "Placement error.");
+    const inline EvaluatorError E0006(6, "Array index error.");
+    const inline EvaluatorError E0007(7, "Limit error.");
+    const inline EvaluatorError E0008(8, "Attribute error.");
+    const inline EvaluatorError E0009(9, "Function error.");
+    const inline EvaluatorError E0010(10, "Control flow error.");
+    const inline EvaluatorError E0011(11, "Memory error.");
+    const inline EvaluatorError E0012(12, "Built-in function error.");
+    const inline EvaluatorError E0013(13, "Ambiguity error.");
 
 }
